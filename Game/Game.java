bdaa3c588aef48a89e86ebb3564bfff85ba1d0a4
aa89227 <aa89227@gmail.com>
2020-06-04 01:30:03 +0800

    aa89227

--- Game/Game.java
+++ Game/Game.java
@@ -1,21 +1,52 @@
 package Game;
 
 import javax.swing.*;
-import java.lang.Thread;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
+import java.lang.Thread;
+import java.awt.Color;
+import java.awt.event.*;
+import java.awt.*;
 public class Game {
     private static JFrame frame = new JFrame("雷霆戰機");
+
+    private static JButton bt_start = new JButton("開始遊戲");
+    private static JButton bt_option = new JButton("選項");
+    private static JButton bt_exit = new JButton("結束遊戲");
+
     private static boolean game_state = false;
     public static void main(String[] args)
     {
         MoveFlight mf = new MoveFlight();
+        frame.setSize(500, 800);
+        frame.setLayout(new GridLayout(1, 3));
         frame.addMouseListener(new MoveFlight());
+
+        bt_start.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent e){start();}
+                }
+            );
+        bt_option.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent e){}
+                }
+            );
+        bt_exit.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent e){System.exit(0);}
+                }
+            );
+            
+        bt_start.setSize(200, 100);
+        bt_option.setSize(200, 100);
+
+        frame.add(bt_start);
+        frame.add(bt_option);
+        frame.add(bt_exit);
         frame.setVisible(true);
     }
 
-    private void start()
+    private static void start()
     {
         //TODO:
         // - sleep 幾秒，顯示起始畫面
@@ -24,13 +55,23 @@ public class Game {
         //      * 跑計時器(計算路程)
         //      * 讀取計時器，到一定的路程會召喚 enemy
         //      * 計算是否戰機有受到傷害
+        return;
+    }
+
+    private static void option()
+    {
 
     }
 
     private void gameover()
     {
         //TODO:
-        //遊戲結束，結算畫面 
+        // - game_state 設為 false
+        // - 顯示結算畫面
+        //      * 分數(計時器)
+        //      * 殺死的 enemy
+        //      * 按鈕 => 回到標題畫面
+        // - 停止計時器
     }
 
     private static class MoveFlight extends MouseAdapter {
